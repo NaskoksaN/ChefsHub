63a21f616027f06879085da116239939d26d70f8
NaskoksaN <nbobchev@yahoo.com>
2024-12-09 21:10:19 +0200

    Create FooterComponent, view

---
+++ Components/FooterComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ChefsHub.Web.Components
+{
+    public class FooterComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
