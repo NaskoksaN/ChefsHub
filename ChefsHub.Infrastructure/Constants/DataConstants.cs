d797ca03c7a359459c883d048b4ad2a8e5a7bed2
NaskoksaN <nbobchev@yahoo.com>
2024-12-16 20:50:11 +0200

    Create DataConstant folder with static file DataConstant

---
+++ ChefsHub.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChefsHub.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int ApplicationUserFirstNameMaxLength = 30;
+        public const int ApplicationUserFirstNameMinLength = 1;
+
+        public const int ApplicationUserLastNameMaxLength = 30;
+        public const int ApplicationUserLastNameMinLength = 1;
+
+       
+    }
+}
