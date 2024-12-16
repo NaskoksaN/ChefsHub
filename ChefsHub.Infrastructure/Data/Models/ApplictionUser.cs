97219593dccdb1ceea452a70787579707173799c
NaskoksaN <nbobchev@yahoo.com>
2024-12-16 20:51:52 +0200

    Create Entity class ApplicationUser to extent identity

---
+++ ChefsHub.Infrastructure/Data/Models/ApplictionUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+using static ChefsHub.Infrastructure.Constants.DataConstants;
+
+namespace ChefsHub.Infrastructure.Data.Models
+{
+    [Comment("Application user data")]
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(ApplicationUserFirstNameMaxLength)]
+        [Comment("Application first name")]
+        [PersonalData]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(ApplicationUserLastNameMaxLength)]
+        [Comment("Application last name")]
+        [PersonalData]
+        public string LastName { get; set; } = string.Empty;
+
+        
+
+    }
+}
