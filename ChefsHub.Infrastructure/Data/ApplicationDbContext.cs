d79ee9fe2278dcd37e5aab1135e551ed3822a41b
NaskoksaN <nbobchev@yahoo.com>
2024-12-16 20:26:21 +0200

    Create ApplicationDbContext in Infrastructure folder

---
+++ ChefsHub.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace ChefsHub.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<string>, string>
+    {
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }   
+}
